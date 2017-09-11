4357d315d1e440aaeb4d8549944e5c3c1b019691
a <a>
2017-09-11 12:32:15 -0400

    9.11 notes

--- starterproject/MyApplication3/app/src/main/java/com/example/leebr1/hello_world/MainActivity.java
+++ starterproject/MyApplication3/app/src/main/java/com/example/leebr1/hello_world/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.leebr1.hello_world;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/*imports for button*/
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, android.net.Uri.parse("http://bxscience.edu"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
