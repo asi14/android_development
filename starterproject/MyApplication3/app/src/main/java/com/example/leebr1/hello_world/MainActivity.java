c76d491a6d46b97520b88ddf324e343c9c024ec1
aaa <aaa>
2017-09-13 12:09:35 -0400

    button layout fixed

--- starterproject/MyApplication3/app/src/main/java/com/example/leebr1/hello_world/MainActivity.java
+++ starterproject/MyApplication3/app/src/main/java/com/example/leebr1/hello_world/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addListenerOnButton();
+        addListenerOnButton2();
     }
 
     public void addListenerOnButton(){
